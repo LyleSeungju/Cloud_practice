c087249558b4ff507940608249a993640a38f78a
김승주 <46523568+LyleSeungju@users.noreply.github.com>
2024-08-17 00:14:35 -0700

    Update app.js

--- BackEnd/nodejs/app.js
+++ BackEnd/nodejs/app.js
@@ -18,9 +18,10 @@ const connection = mysql.createConnection({
 connection.connect((err) => {
   if (err) {
     console.error('Error connecting to the database:', err);
-    return;
+    process.exit(1); // 데이터베이스 연결에 실패하면 애플리케이션을 종료합니다.
+  } else {
+    console.log('Connected to the database.');
   }
-  console.log('Connected to the database.');
 });
 
 // 기본 라우트
@@ -28,19 +29,19 @@ app.get('/getData', (req, res) => {
   res.send('Hello from Node.js!');
 });
 
+// 데이터 조회 라우트
 app.get('/data', (req, res) => {
   const query = 'SELECT * FROM new_table';
-  
+
   connection.query(query, (err, results) => {
     if (err) {
       console.error('쿼리 실행 오류:', err);
       res.status(500).send('서버 오류');
       return;
     }
-    res.json(results);
+    res.json(results); // 조회된 데이터를 클라이언트에게 JSON 형식으로 반환합니다.
   });
 });
-
 
 // 서버 시작
 const port = 8080;
